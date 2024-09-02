8f80511a327d752f35cb946ac746299fab1d8bf3
Manellze <manoelmsrtinssantosneto@Gmail.com.com>
2024-09-02 15:56:47 -0300

    atualização

--- script.js
+++ script.js
@@ -72,10 +72,10 @@ function gerarPDF() {
 
     // Adicione a imagem da logo
 const logoDataUrl = 'img/AC_40ANOS.png'; // Substitua pelo seu URL ou Base64 da logo
-const logoWidth = 60; // Ajuste conforme o tamanho da sua logo
-const logoHeight = 40; // Ajuste conforme o tamanho da sua logo
-const logoX = -2; // Posição X da logo
-const logoY = startY - 48; // Posição Y da logo
+const logoWidth = 90; // Ajuste conforme o tamanho da sua logo
+const logoHeight = 60; // Ajuste conforme o tamanho da sua logo
+const logoX = -22; // Posição X da logo
+const logoY = startY - 50; // Posição Y da logo
 
 doc.addImage(logoDataUrl, 'PNG', logoX, logoY, logoWidth, logoHeight);
 
@@ -87,7 +87,7 @@ doc.addImage(logoDataUrl, 'PNG', logoX, logoY, logoWidth, logoHeight);
 
     // Configuração do título
     doc.setFontSize(16);
-    doc.text('Dados de Cadastro', pageWidth / 2, startY + -25 , null, null, 'center');
+    doc.text('Dados de Cadastro', pageWidth / 2, startY + -18 , null, null, 'center');
 
     // Desenho de borda ao redor dos dados
     doc.setLineWidth(0.5);
@@ -143,34 +143,41 @@ doc.text(`${dados.data.getDate().toString().padStart(2, '0')}/${(dados.data.getM
     doc.text('(  )Gmail   (  )Asana (  )Sienge (  )Construtor de Vendas', pageWidth / 2, startY + 105, null, null, 'center');
     doc.text('', pageWidth / 2, startY + 110, null, null, 'center');
 
-    // Assinaturas
+    // Passo a passo
+    const steps = [
+        '1. Faça login no notebook usando o logon e a senha provisória fornecidos. No primeiro acesso, você precisará criar uma senha pessoal.',
+        '2. Faça o mesmo no Email, utilizando seu email e sua senha provisória.',
+        '3. Informações sobre seus outros acessos serão enviadas por email.'
+    ];
+
+    doc.setFontSize(12);
+    doc.text('Passo a Passo:', 10, startY + 140);
+    steps.forEach((step, index) => {
+        doc.text(`${step}`, 15, startY + 150 + index * 10);
+    });
+
     const assinaturaWidth = 60;
     const gap = 10;
     const x1 = (pageWidth - 3 * assinaturaWidth - 2 * gap) / 2;
-    const assinaturaY = startY + 160;
-
+    const assinaturaY = startY + 210;
+    const textOffset = 5; // Ajuste para garantir que o texto fique exatamente abaixo da linha
+    const emailEmpresa = 'contato@acengenhariase.com.br'; // Email da empresa
+    
     // Linha para assinatura do Setor Responsável
-    doc.line(x1, assinaturaY, x1 + assinaturaWidth, assinaturaY);
-    doc.text('Ass. do Setor Responsável', x1 + assinaturaWidth / 2, assinaturaY + 10, null, null, 'center');
-
+    doc.line(x1, assinaturaY + textOffset, x1 + assinaturaWidth, assinaturaY + textOffset);
+    doc.text('Ass. do Setor Responsável', x1 + assinaturaWidth / 2, assinaturaY + textOffset + 10, null, null, 'center');
+    
     // Linha para assinatura do Colaborador
-    doc.line(x1 + assinaturaWidth + gap, assinaturaY, x1 + 2 * assinaturaWidth + gap, assinaturaY);
-    doc.text('Ass. do Colaborador', x1 + assinaturaWidth + gap + assinaturaWidth / 2, assinaturaY + 10, null, null, 'center');
-
+    doc.line(x1 + assinaturaWidth + gap, assinaturaY + textOffset, x1 + 2 * assinaturaWidth + gap, assinaturaY + textOffset);
+    doc.text('Ass. do Colaborador', x1 + assinaturaWidth + gap + assinaturaWidth / 2, assinaturaY + textOffset + 10, null, null, 'center');
+    
     // Linha para assinatura do Gestor
-    doc.line(x1 + 2 * (assinaturaWidth + gap), assinaturaY, x1 + 3 * assinaturaWidth + 2 * gap, assinaturaY);
-    doc.text('Ass. do Gestor', x1 + 2 * assinaturaWidth + 2 * gap + assinaturaWidth / 2, assinaturaY + 10, null, null, 'center');
+    doc.line(x1 + 2 * (assinaturaWidth + gap), assinaturaY + textOffset, x1 + 3 * assinaturaWidth + 2 * gap, assinaturaY + textOffset);
+    doc.text('Ass. do Gestor', x1 + 2 * assinaturaWidth + 2 * gap + assinaturaWidth / 2, assinaturaY + textOffset + 10, null, null, 'center');
 
-    // Adicione a frase de boas-vindas abaixo das assinaturas
-const welcomeMessageY = assinaturaY + 60; // Ajuste a posição vertical do texto de boas-vindas
-const welcomeMessage = 'Bem-vindos à empresa! Passo 1: Faça login no notebook usando o logon e a senha provisória fornecidos ( No primeiro acesso, você precisará criar uma senha pessoal ). Passo 2: faça o mesmo no Email, utilize seu email e sua senha provisória. Passo 3: Informações sobre seus outros acessos serão enviados por email.';
-
+    const emailY = assinaturaY + 40; // Ajuste a posição vertical conforme necessário
 doc.setFontSize(12);
-doc.text(welcomeMessage, 10, welcomeMessageY, { maxWidth: pageWidth - 20 });
-const lines = doc.splitTextToSize(welcomeMessage, pageWidth - 20);
-
-// Adicione o texto dividido no PDF
-doc.text(lines, 10, welcomeMessageY);
+doc.text(emailEmpresa, pageWidth / 2, emailY, null, null, 'center');
 
     // Salvar o PDF com o nome do logon
     doc.save(`${dados.logon}.pdf`);
